e6e30ea89e9ba6cba4b4ef0cb6fe232cf4003a25
Jose <jose_esteban_basualdo@hotmail.com>
2021-11-02 00:17:52 -0300

    agrega boton comprar
    Este boton será utilizado para generar un model con la lista de lo que se compró

--- js/funciones.js
+++ js/funciones.js
@@ -120,6 +120,29 @@ function mostrarTotalGastado() {
 
       <button type="button" class="btn btn-danger dolar">Pasar total a USD</button>
       <button type="button" class="btn btn-info pesos">Pasar total a Pesos</button>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Comprar
+      </button>
+
+      <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     
       <p class="parrafo">La cantidad de productos es : ${productosTotal}
       y el valor total es  $: ${precioTotal.toFixed(2)}
